e2fc881565bc166ddd263ee88ac88fb13037580b
thana <mrrhello894@gmail.com>
2026-07-09 15:27:52 +0700

    update Project

--- js/main.js
+++ js/main.js
@@ -1,31 +1,46 @@
+const navbar = document.getElementById("navbar");
 const menuBtn = document.getElementById("menu-btn");
 const mobileMenu = document.getElementById("mobile-menu");
+const htmlEl = document.getElementById('root-html');
+const themeBtns = [document.getElementById('theme-toggle-btn'), document.getElementById('mobile-theme-toggle-btn')];
+const sunIcon = document.getElementById('sun-icon');
+const moonIcon = document.getElementById('moon-icon');
 
-menuBtn.addEventListener("click", () => {
-  mobileMenu.classList.toggle("hidden");
+menuBtn?.addEventListener("click", () => {
+    mobileMenu?.classList.toggle("hidden");
 });
 
-const navbar = document.getElementById("navbar");
-
-let lastScroll = 0;
-
+let lastScrollY = window.scrollY;
 window.addEventListener("scroll", () => {
-  const currentScroll = window.pageYOffset;
-
-  if (currentScroll <= 0) {
-    navbar.classList.remove("-translate-y-full");
-    return;
-  }
-  if (
-    currentScroll > lastScroll &&
-    !navbar.classList.contains("-translate-y-full")
-  ) {
-    navbar.classList.add("-translate-y-full");
-  } else if (
-    currentScroll < lastScroll &&
-    navbar.classList.contains("-translate-y-full")
-  ) {
-    navbar.classList.remove("-translate-y-full");
-  }
-  lastScroll = currentScroll;
+    if (!navbar) return;
+    const currentScrollY = window.scrollY;
+    
+    if (currentScrollY <= 0) {
+        navbar.classList.remove("-translate-y-full");
+    } else if (Math.abs(currentScrollY - lastScrollY) > 10) {
+        navbar.classList.toggle("-translate-y-full", currentScrollY > lastScrollY);
+    }
+    lastScrollY = currentScrollY;
 });
+
+const updateIcons = (isDark) => {
+    sunIcon?.classList.toggle('hidden', !isDark);
+    moonIcon?.classList.toggle('hidden', isDark);
+};
+
+const toggleTheme = () => {
+    const isDark = htmlEl.classList.toggle('dark');
+    localStorage.setItem('theme', isDark ? 'dark' : 'light');
+    updateIcons(isDark);
+};
+const isSystemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const isDark = localStorage.theme === 'dark' || (!('theme' in localStorage) && isSystemDark);
+
+if (isDark) {
+    htmlEl.classList.add('dark');
+} else {
+    htmlEl.classList.remove('dark');
+}
+updateIcons(isDark);
+
+themeBtns.forEach(btn => btn?.addEventListener('click', toggleTheme));
